79689b75697653120ed5f18f13e6615aca41eb8e
xiongyidi <1098476525@qq.com>
2020-06-24 23:15:19 +0800

    modified

---
+++ TheFirstModule/src/LeetCode/Solution125.java
@@ -0,0 +1,32 @@
+package LeetCode;
+/*
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+示例 2:
+
+输入: "race a car"
+输出: false
+ */
+public class Solution125 {
+    //判断是否为数字或者小写字母： Character.isLetterOrDigit(ch)
+    //大小写转小写字母： Character.toLowerCase(ch)
+    //字符串反转： StringBuffer_.reverse()
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        StringBuffer sgood_rev = new StringBuffer(sgood).reverse();
+        return sgood.toString().equals(sgood_rev.toString());
+    }
+}
