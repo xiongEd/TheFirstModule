62a37472ab851bd4a6aeb5e4be13de2b5a95dd26
xiongyidi <1098476525@qq.com>
2020-07-02 23:58:39 +0800

    solution 378   718

---
+++ TheFirstModule/src/LeetCode/Solution718.java
@@ -0,0 +1,51 @@
+package LeetCode;
+/*
+给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+
+ 
+
+示例：
+
+输入：
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+输出：3
+解释：
+长度最长的公共子数组是 [3, 2, 1] 。
+ 
+
+提示：
+
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+ */
+public class Solution718 {
+    public int findLength(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            int len = Math.min(m, n - i);
+            int maxlen = maxLength(A, B, i, 0, len);
+            ret = Math.max(ret, maxlen);
+        }
+        for (int i = 0; i < m; i++) {
+            int len = Math.min(n, m - i);
+            int maxlen = maxLength(A, B, 0, i, len);
+            ret = Math.max(ret, maxlen);
+        }
+        return ret;
+    }
+
+    public int maxLength(int[] A, int[] B, int addA, int addB, int len) {
+        int ret = 0, k = 0;
+        for (int i = 0; i < len; i++) {
+            if (A[addA + i] == B[addB + i]) {
+                k++;
+            } else {
+                k = 0;
+            }
+            ret = Math.max(ret, k);
+        }
+        return ret;
+    }
+}
